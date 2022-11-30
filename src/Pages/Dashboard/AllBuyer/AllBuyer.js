eb64ca04d5e941ff8d048e884086a35d52f6a413
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-30 12:18:40 +0600

    all buyer part done.

--- src/Pages/Dashboard/AllBuyer/AllBuyer.js
+++ src/Pages/Dashboard/AllBuyer/AllBuyer.js
@@ -1,9 +1,77 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+
 
 const AllBuyer = () => {
+
+    const url = `http://localhost:5000/allbuyers`;
+
+    const { data: buyers = null, refetch } = useQuery({
+        queryKey: ['allbuyers'],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+    })
+    
+    const deleteBuyer = (id) => {
+        const data = {
+            id: id
+        }
+
+        fetch(`http://localhost:5000/deletebuyer`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Delete Successfully!')
+                }
+
+                refetch()
+            })
+
+    }
     return (
-        <div>
-            
+        <div className='w-[98%] mx-auto'>
+            <h3 className='text-3xl mb-6 py-6 font-bold text-center'>All Buyers</h3>
+            {
+                buyers && buyers?.length > 0 ? <div className="overflow-x-auto">
+                    <table className="table w-full">
+
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <td>Type</td>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                buyers?.map((buyer, i) =>
+                                    <tr key={buyer._id}>
+                                        <th>{i + 1}</th>
+                                        <td>{buyer.name}</td>
+                                        <td>{buyer.email}</td>
+                                        <td>{buyer.type}</td>
+                                    
+                                        <td><button onClick={() => deleteBuyer(buyer._id)} className='btn btn-warning btn-sm '>Delete</button></td>
+                                    </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                    :
+                    <p className='text-center text-2xl font-semibold'>No Products found</p>
+            }
         </div>
     );
 };
