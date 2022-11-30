fc446706f0884e58c4413bd3e5763033e404902f
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-30 17:46:41 +0600

    extra section added.

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider';
 import useUserType from '../../../Hooks/useUserType';
 import AdvertisedItems from '../AdvertisedItems/AdvertisedItems';
@@ -14,6 +15,19 @@ const Home = () => {
             <Banner></Banner>
             <ProductCategories></ProductCategories>
             <AdvertisedItems></AdvertisedItems>
+            <div className="hero px-4 py-10 bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-8">
+                    <img src={`https://img.freepik.com/free-vector/online-shop-cartoon-poster-hand-give-shopping-bag_107791-7042.jpg?w=2000`} className="rounded-lg shadow-2xl lg:w-[50%] h-full" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Sell and Buy Mobile.</h1>
+                        <p className="py-6">Sell your mobile phone for cash!
+                            Get the most cash for your phone.Mobile phone recycling refers to the process to trading in your phone, so it can either be recycled for parts or refurbished for resale.
+                            Even older phones contain valuable parts and materials that can be reused. Recycling companies will extract these often-toxic materials in a way that's safe and environmentally friendly.
+                            Meanwhile, by trading in your phone to be recycled you'll get some cash to spend while doing your bit for the environment.</p>
+                        <Link to='/' className="btn btn-primary">Get Started</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
